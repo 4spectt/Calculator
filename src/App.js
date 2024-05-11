6d69f85f57c0cb2237f5c1754e5d035b8120a643
Marc Angelo Caparoso <angelocaparoso801@gmail.com>
2024-05-11 16:47:48 +0800

    Created a CSS file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './style.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+      <input type="text" id="display" disabled />
+      <div className="row">
+        <input type="button" value="7" />
+        <input type="button" value="8" />
+        <input type="button" value="9" />
+        <input type="button" value="/" />
+      </div>
+      <div className="row">
+        <input type="button" value="4" />
+        <input type="button" value="5" />
+        <input type="button" value="6" />
+        <input type="button" value="-" />
+      </div>
+      <div className="row">
+        <input type="button" value="1" />
+        <input type="button" value="2" />
+        <input type="button" value="3" />
+        <input type="button" value="+" />
+      </div>
+      <div className="row">
+        <input type="button" value="C" />
+        <input type="button" value="0" />
+        <input type="button" value="=" />
+        <input type="button" value="*" />
+      </div>
+      <div className="row">
+        <input type="button" value="Reset" />
+      </div>
     </div>
   );
 }
